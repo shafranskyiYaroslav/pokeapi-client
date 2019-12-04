01ef8ffa583d22109199eff7363fd0bf4939c9ae
ShafranskyiYaroslav <yshafranskyi@intellias.com>
2019-12-04 17:18:15 +0200

    Updates

---
+++ precache-manifest.ae50801c7ce5c389955cdd9104c410e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e408d095759fcbb62bfbc7a7f3ef1ad9",
+    "url": "/pokeapi-client/index.html"
+  },
+  {
+    "revision": "da737a7fe4e62e5cbe0b",
+    "url": "/pokeapi-client/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "d2140db4dee388751e41",
+    "url": "/pokeapi-client/static/css/main.de9ee101.chunk.css"
+  },
+  {
+    "revision": "da737a7fe4e62e5cbe0b",
+    "url": "/pokeapi-client/static/js/2.59fdd037.chunk.js"
+  },
+  {
+    "revision": "d2140db4dee388751e41",
+    "url": "/pokeapi-client/static/js/main.a49c20da.chunk.js"
+  },
+  {
+    "revision": "2661c479249c04a4657c",
+    "url": "/pokeapi-client/static/js/runtime-main.82d954ef.js"
+  }
+]);
